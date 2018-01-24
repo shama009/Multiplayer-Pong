86137a981772a9553a12536d255910904fed1a3b
Ferny Dany <fernydany@Fernys-MacBook-Pro.local>
2018-01-24 15:44:59 -0800

    Update

--- server.js
+++ server.js
@@ -281,7 +281,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 // app.use(express.static(path.join(public)));
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 // app.use('/', index);
 
