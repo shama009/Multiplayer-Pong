e5020b6195276f1acf3d4ca23e857d6a54f98997
Ferny Dany <fernydany@Fernys-MacBook-Pro.local>
2018-01-24 15:14:46 -0800

    bug-fix missing phaser plugin

--- server.js
+++ server.js
@@ -280,8 +280,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname)));
-app.use(express.static(__dirname));
+// app.use(express.static(path.join(public)));
+app.use(express.static('public'));
 
 // app.use('/', index);
 
